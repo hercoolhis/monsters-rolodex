8d645e09510dcc0f6a386f91a255e3a71dfe181a
Sulaiman Agbenla <agbenla.lanre@gmail.com>
2020-01-12 07:45:30 +0100

    Updates

---
+++ precache-manifest.741cc9167de5f06618a2260a164dc0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399914db7170fa076d8f35b6cc8e6234",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7705746fca64736572b8",
+    "url": "/monsters-rolodex/static/css/main.c0cc41f4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7705746fca64736572b8",
+    "url": "/monsters-rolodex/static/js/main.e4ab1aed.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
